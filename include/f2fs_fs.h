c4c38b9a39736c92a3415f34898d94468683ea3a
Chao Yu <yuchao0@huawei.com>
2019-04-23 10:42:20 +0800

    fsck.f2fs: fix to repair ro mounted device w/ -f
    As Hagbard Celine reported:
    
    "
    Referring to the output from the fsck running against a "ro"
    filesystem, especially this line:
    Info: Check FS only due to RO
    
    As far as i can tell this says that opposed to other filesystems
    running fsck against a "ro" mounted f2fs partition will never fix any
    errors.
    So I tried running fsck against the same partition mounted "rw":
    - mount -o remount,rw /mnt/f2fstest/
    - fsck.f2fs  -f /dev/nvme0n1p7
    Info: Force to fix corruption
    Info: Mounted device!
            Error: Not available on mounted device!
    
    I might be misunderstanding something, but all this tells me that
    unless one make a custom initramfs that runs fsck before root is
    mounted (something no distributions has, as far as I know), fsck will
    never fix an f2fs formatted root partition during boot.
    If this is by design and not a bug/unintended behavior, it should be
    documented somewhere least more people will experience system crashes
    like mine.
    
    All tests above done with kernel 5.0.5 and f2fs-tools 1.12.0 with
    "fsck.f2fs: allow to fsck readonly image w/ -f option"-patch by Chao
    Yu.
    "
    
    We try to make our fsck behavior keeping line with e2fsprogs, but w/
    -f option, we can just check a RO mounted device rather repair it, so
    let's fix this.
    
    Reported-and-Tested-by: Hagbard Celine <hagbardcelin@gmail.com>
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- include/f2fs_fs.h
+++ include/f2fs_fs.h
@@ -1156,6 +1156,7 @@ extern int f2fs_crc_valid(u_int32_t blk_crc, void *buf, int len);
 
 extern void f2fs_init_configuration(void);
 extern int f2fs_devs_are_umounted(void);
+extern int f2fs_dev_is_writable(void);
 extern int f2fs_dev_is_umounted(char *);
 extern int f2fs_get_device_info(void);
 extern int get_device_info(int);
